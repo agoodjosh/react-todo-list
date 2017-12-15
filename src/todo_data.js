7a7adbeaf64cfec165c29c736b24d8fc57048781
jauxsh <joshua.jih.chang@gmail.com>
2017-12-14 18:10:21 -0800

    finished base to-do list

---
+++ src/todo_data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Buy Presents',
+        details: 'for Jane (jason mark) and Justin (some gift...)',
+        complete: false
+    },
+    {
+        title: 'Learn React',
+        details: 'gah',
+        complete: false
+    },
+    {
+        title: 'fix style-images prototype',
+        details: 'images are not scattered throughout window',
+        complete: false
+    },
+    {
+        title: 'meet with Notes Group',
+        details: 'gotta think of a name for the app',
+        complete: false
+    }
+];
