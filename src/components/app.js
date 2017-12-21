3909bac828499e0800a8fd49a54f9bc89d58d1a9
jauxsh <joshua.jih.chang@gmail.com>
2017-12-20 17:42:06 -0800

    installed axios, methods to get delete complete data

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,19 @@
 import 'materialize-css/dist/css/materialize.min.css'
 import React, {Component} from 'react';
-import todo_data from "../todo_data";
 import AddItem from './add_item';
 import ListContainer from './list_container';
 
+import axios from 'axios';
+
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=jauxshskey';
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            todoData: todo_data
+            todoData: []
         };
 
         this.addItem = this.addItem.bind(this);
@@ -18,30 +21,51 @@ class App extends Component {
         this.toggleComplete = this.toggleComplete.bind(this);
     }
 
-    addItem(item) {
+    componentDidMount() {
+        this.getData();
+
+    }
+
+    async getData() {
+        const resp = await axios.get(`${BASE_URL}/todos${API_KEY}`);
+
+        this.setState({
+            todoData: resp.data.todos
+        })
+        //     .then( (resp) => {
+        //     console.log('Response', resp);
+        //     this.setState({
+        //         todoData: resp.data.todos
+        //     })
+        // } );
+    }
+
+    async addItem(item) {
         item.complete = false;
-        this.setState({
-            todoData: [item, ...this.state.todoData]
-        })
+
+        await axios.post(`${BASE_URL}/todos${API_KEY}`, item);
+
+        this.getData();
+
+        //     .then( (resp) => {
+        //     console.log('Add - Resp: ', resp);
+        //
+        //     this.getData();
+        // });
     }
 
-    toggleComplete(index) {
-        const tempData = this.state.todoData.slice();
-        tempData[index].complete = !tempData[index].complete;
-        this.setState({
-            todoData: tempData
-        })
+    async toggleComplete(id) {
+        await axios.put(`${BASE_URL}/todos/${id + API_KEY}`);
+
+        this.getData();
     }
 
-    deleteItem(index) {
-        const tempData = this.state.todoData.slice();
+    async deleteItem(id) {
+        await axios.delete(`${BASE_URL}/todos/${id + API_KEY}`);
 
-        tempData.splice(index, 1);
-
-        this.setState({
-           todoData: tempData
-        });
+        this.getData();
     }
+
 
     render() {
         const { todoData } = this.state;
